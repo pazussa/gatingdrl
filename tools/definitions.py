4c93a06952df81f839b12076a557cb5c737b93c8
Juan Paz <pazussa@gmail.com>
2025-05-20 14:06:04 -0500

    out

--- tools/definitions.py
+++ tools/definitions.py
@@ -3,6 +3,9 @@ import os
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # newtas root directory
 
 OUT_DIR = os.path.join(ROOT_DIR, 'out')
+# Create the output directory if it doesn't exist
+os.makedirs(OUT_DIR, exist_ok=True)
+
 CONFIG_DIR = os.path.join(ROOT_DIR, 'config')
 
 LOG_DIR = os.path.join(OUT_DIR, 'log')
